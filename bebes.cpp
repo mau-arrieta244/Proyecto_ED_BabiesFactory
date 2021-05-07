af3e6418be67b7eaf6a8fbf0f029cfbc962ac678
Mauricio Arrieta <mauarrieta24@gmail.com>
2021-05-06 21:19:37 -0600

    corro funcion listdir desde main

--- bebes.cpp
+++ bebes.cpp
@@ -2,14 +2,21 @@
 //Mauricio Arrieta , Gabriel Fallas
 //Estructuras de Datos, grupo 3
 
+
+// tambien incluye struct colaPedidos, deberia cambiarle el nombre a estructuras.h 
+// cuando unamos todas las estructuras en este.
+
+//para unir las estructuras en uno solo, debemos cambiar nombres de Nodo y Cola a mas especificos
+
+
 #include <iostream>
 #include <string>
 #include<fstream>
 #include<dirent.h>
 #include<cstdio>
+#include "padres.cpp"
 
-#include<stdlib.h> // para los randoms de probabilidades
-#include<time.h>
+
 
 #include<sstream> //string to stream para getline()
 
@@ -29,7 +36,7 @@ string colorPapa(string nombreArchivo){
     char separador(';'); // siempre puntos y comas
     getline(stream,tipoPadre,separador);
 
-    cout<<"tipo padre: "<<tipoPadre<<endl;
+    //cout<<"\ntipo padre: "<<tipoPadre<<endl;
     return tipoPadre;
     
 }
@@ -47,9 +54,10 @@ int longitudPapa(string nombreArchivo){
     char separador(';'); // siempre puntos y comas
     getline(stream,tipoPadre,separador);
 
-    cout<<"longitud: "<<tipoPadre.length()<<endl;
+    //cout<<"longitud: "<<tipoPadre.length()<<endl;
     return tipoPadre.length();
 }
+
 
 string colorMama(string nombreArchivo,int longitudPadre){
     ifstream file(nombreArchivo);
@@ -58,8 +66,9 @@ string colorMama(string nombreArchivo,int longitudPadre){
     while(file>>input){
         contenido+=input;
     }
+    //que pasa si le toca Bad Bunny? lo retorna como "BadBunny" (corta el whitespace)
     string tipoMadre = contenido.substr(longitudPadre+1);
-    cout<<"tipo madre: "<<tipoMadre<<endl;
+    //cout<<"\ntipo madre: "<<tipoMadre<<endl;
     return tipoMadre;
     
 }
@@ -71,259 +80,7 @@ string checkPathString(string path){
     return path;
 }
 
-struct Padres{ // será lo que contiene cada "casilla" en la matriz para creacion por pedido especial
-    string tipoPadre;
-    string tipoMadre;
-    Padres(){
-        tipoPadre = "";
-        tipoMadre = "";
-    }
-    Padres(string colorPadre , string colorMadre){
-        tipoPadre = colorPadre;
-        tipoMadre = colorMadre;
-    }
-    string probabilidadHijo(){
-        srand(time(nullptr));
-        int aleatorio = rand() %100+1;
-        //cout<<"random : "<<aleatorio<<endl;
 
-        //hay 64 combinaciones... muchos ifs, elifs!
-
-        //preguntarle bien al profe de manera de escribir castano , BadBunny, etc..
-
-        // columna 1 /////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////
-        if(tipoPadre == "Moreno" & tipoMadre == "Morena"){
-            return "Moreno";
-        }
-
-        else if(tipoPadre == "Afro" & tipoMadre == "Morena"){
-            if(aleatorio<=70){
-                return "Afro";
-            }
-            else{
-                return "Moreno";
-            }
-        }
-
-        else if(tipoPadre == "Rubio" & tipoMadre == "Morena"){
-            if(aleatorio<=90){
-                return "Moreno";
-            }
-            else{
-                return "Rubio";
-            }
-        }
-
-        else if(tipoPadre == "Castano" & tipoMadre == "Morena"){
-            if(aleatorio<=50){
-                return "Castano";
-            }
-            else{
-                return "Moreno";
-            }
-        }
-
-        else if(tipoPadre == "Pelirrojo" & tipoMadre == "Morena"){
-            if(aleatorio<=10){
-                return "Pelirrojo";
-            }
-            else{
-                return "Moreno";
-            }
-        }
-
-        else if(tipoPadre == "Pink" & tipoMadre == "Morena"){
-            return "Moreno";
-        }
-
-        else if(tipoPadre == "BadBunny" & tipoMadre == "Morena"){
-            if(aleatorio<=10){
-                return "BadBunny";
-            }
-            else{
-                return "Moreno";
-            }
-        }
-
-        else if(tipoPadre == "Pitufo" & tipoMadre == "Morena"){
-            if(aleatorio<=10){
-                return "Pitufo";
-            }
-            else{
-                return "Moreno";
-            }
-        }
-
-        // columna 2 /////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////
-
-        else if(tipoPadre == "Moreno" & tipoMadre == "Afro"){
-            if(aleatorio<=30){
-                return "Moreno";
-            }
-            else{
-                return "Afro";
-            }
-        }
-
-        else if(tipoPadre == "Afro" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        else if(tipoPadre == "Rubio" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        else if(tipoPadre == "Castano" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        else if(tipoPadre == "Pelirrojo" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        else if(tipoPadre == "Pink" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        else if(tipoPadre == "BadBunny" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        else if(tipoPadre == "Pitufo" & tipoMadre == "Afro"){
-            return "Afro";
-        }
-
-        // columna 3 /////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////
-        else if(tipoPadre == "Moreno" & tipoMadre == "Rubia"){
-            if(aleatorio<=70){
-                return "Moreno";
-            }
-            else{
-                return "Rubia";
-            }
-        }
-
-        else if(tipoPadre == "Afro" & tipoMadre == "Rubia"){
-            if(aleatorio<=90){
-                return "Afro";
-            }
-            else{
-                return "Rubia";
-            }
-        }
-
-        else if(tipoPadre == "Rubio" & tipoMadre == "Rubia"){
-            return "Rubio";
-        }
-
-        else if(tipoPadre == "Castano" & tipoMadre == "Rubia"){
-            if(aleatorio<=90){
-                return "Rubio";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "Pelirrojo" & tipoMadre == "Rubia"){
-            if(aleatorio<=90){
-                return "Rubio";
-            }
-            else{
-                return "Pelirrojo";
-            }
-        }
-
-        else if(tipoPadre == "Pink" & tipoMadre == "Rubia"){
-            return "Rubia";
-        }
-
-        else if(tipoPadre == "BadBunny" & tipoMadre == "Rubia"){
-            return "Rubia";
-        }
-
-        else if(tipoPadre == "Pitufo" & tipoMadre == "Rubia"){
-            return "Rubia";
-        }
-
-        // columna 4 /////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////
-
-        else if(tipoPadre == "Moreno" & tipoMadre == "Castana"){
-            if(aleatorio<=50){
-                return "Moreno";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "Afro" & tipoMadre == "Castana"){
-            if(aleatorio<=70){
-                return "Afro";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "Rubio" & tipoMadre == "Castana"){
-            if(aleatorio<=40){
-                return "Rubio";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "Castano" & tipoMadre == "Castana"){
-            return "Castano";
-        }
-
-        else if(tipoPadre == "Pelirrojo" & tipoMadre == "Castana"){
-            if(aleatorio<=40){
-                return "Pelirrojo";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "Pink" & tipoMadre == "Castana"){
-            if(aleatorio<=30){
-                return "Pink";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "BadBunny" & tipoMadre == "Castana"){
-            if(aleatorio<=50){
-                return "BadBunny";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        else if(tipoPadre == "Pitufo" & tipoMadre == "Castana"){
-            if(aleatorio<=30){
-                return "Pitufo";
-            }
-            else{
-                return "Castano";
-            }
-        }
-
-        return "Default / Fallo";
-
-    }
-
-};
 struct Bebe{
 
     string amor;
@@ -341,17 +98,12 @@ struct Bebe{
         bebeMalo = false;
     }
     //constructor cuando se ingresan string papá y mamá
-    Bebe(string colorMama,string colorPapa){
+    Bebe(string color){
         amor = "";
         arte = "";
         hate = "";
         bebeMalo = false;
-        if(colorMama == "Morena"&colorPapa == "Moreno"){
-            colorPiel = "Moreno";
-        }
-        else{
-            colorPiel = "Default";
-        }
+        colorPiel = color;
             
         
     }
@@ -371,16 +123,241 @@ struct Bebe{
             colorPiel = "Default";
     }
     void imprimirPiel(){
-        cout<<"\n ------------------------------ \n"<<endl;
+        
         cout<<"\ncolor de piel: "<<colorPiel<<endl;
+        cout<<"\n ------------------------------ \n"<<endl;
     }
 
 };
 
-void listdir(string initStrPath){
+
+//Nodos para colaPedidos , los nodos seran bebes que estan a la espera
+//de pasar a la cola de produccion
+
+struct NodoBebe {
+       int dato; // parte de datos
+       Bebe * bebe;  //TODO: CHANGE PRIORITY
+       NodoBebe* siguiente;// puntero para enlazar nodos
+       // constructor
+       
+       NodoBebe() 
+       { 
+            siguiente = NULL; // sig es null
+       }
+
+       //TODO: CHANGE PRIORITY       
+       NodoBebe(Bebe * bebeIngresado) 
+       {
+            siguiente = NULL; // sig es null
+            bebe = bebeIngresado;
+                
+       }
+      void imprimir();
+       
+};
+
+
+struct ColaPedidos {
+       // solo con pN es suficiente
+       NodoBebe *frente; // ERROR sin ultimo nodo
+       
+       ColaPedidos()
+       {
+            frente = NULL;
+       }
+       
+
+       void encolarBebe (Bebe * b){
+       		if (vacia())
+		        frente = new NodoBebe (b);
+		    else 
+		    {
+		            // referencia al primero para recorrer la lista
+		            NodoBebe* actual = frente;
+		            // recorre la lista hasta llegar al penultimo nodo
+		            while (actual->siguiente != NULL)
+		                          actual = actual->siguiente; 
+		              
+		            // Crea nuevo nodo, lo apunta con uN  
+		            NodoBebe* nuevo = new NodoBebe (b);
+		            //le quita el enlace al que era ultimo
+		            actual->siguiente = nuevo;
+		    }
+	   }
+
+
+       NodoBebe * desencolar()
+       {
+      // si no hay elementos, no borra nada, retorna null
+      if (vacia())
+      {
+         return NULL;
+      }
+      else
+            {
+          // un puntero que apunta al nodo que se
+          // "despegara" de la lista y se retorna
+          NodoBebe* borrado = frente;     
+          // pN se pasa al segundo de la lista
+          // porque el primero se eliminara
+          frente = frente->siguiente;
+          // el nodo borrado, se debe despegar
+          // entonces siguiente apunta a null y no
+          // al segundo de la lista que ahora es pN
+          borrado->siguiente = NULL;
+          // aca se deberia liberar la memoria si no se
+          // retornara el borrado
+          return borrado;
+            }
+       }
+
+    bool vacia()
+    {
+     if (frente == NULL)
+        return true;
+     else
+         return false;
+    }
+
+    void imprimir()
+    {
+     cout << "Frente" << endl;
+     // puntero temporal que avanza
+     // por toda la lista
+     // apunta a los mismo que pN
+     NodoBebe *tmp = frente;
+     
+     // mientras tmp no sea nulo, avanza
+     while (tmp != NULL)
+     {
+           // imprime el dato del nodo en el que esta tmp
+           cout << tmp->bebe->colorPiel << "  " << endl;
+           // tmp avanza al siguiente nodo
+           // esto no puede faltar porque se encicla
+           tmp = tmp->siguiente;
+     }
+     cout << "Final" << endl;
+    }
+
+
+    NodoBebe* verFrente()
+        {
+            return frente;      
+        }
+};
+
+//Nodo para lista doble del historico de los pedidos especiales
+struct NodoHistorico{
+	string tipoMadre;
+    string tipoPadre;
+    string tipoBebe;
+
+	NodoHistorico * siguiente;
+	NodoHistorico * anterior;
+	
+	NodoHistorico(){
+		tipoMadre = "";
+        tipoPadre = "";
+        tipoBebe = "";
+		siguiente = anterior = NULL;
+	}
+	
+	NodoHistorico(string mama , string papa, string bebe){
+		tipoMadre = mama;
+        tipoPadre = papa;
+        tipoBebe = bebe;
+		siguiente = anterior = NULL;
+	}
+};
+
+//lista doble para guardar historico de los pedidos especiales
+struct ListaPedidosEspeciales{
+	NodoHistorico * primerNodo, * ultimoNodo;
+    int cantidadBebes=0;
+	
+	ListaPedidosEspeciales(){
+		primerNodo = ultimoNodo = NULL;
+	}
+	
+	bool isEmpty(){
+		return primerNodo == NULL;
+	}
+	
+	void insertar (string infoMama,string infoPapa,string infoHijo){
+		if (isEmpty())
+			primerNodo = ultimoNodo = new NodoHistorico(infoMama,infoPapa,infoHijo);
+		else{
+			primerNodo->anterior = new NodoHistorico(infoMama,infoPapa,infoHijo);
+			primerNodo->anterior->siguiente = primerNodo;
+			primerNodo = primerNodo->anterior;
+		}
+        cantidadBebes++;		
+	}
+	
+	void insertarAlFinal(string infoMama,string infoPapa,string infoHijo){
+		if (isEmpty())
+			primerNodo = ultimoNodo = new NodoHistorico(infoMama,infoPapa,infoHijo);
+		else{
+			ultimoNodo->siguiente = new NodoHistorico(infoMama,infoPapa,infoHijo);
+			ultimoNodo->siguiente->anterior = ultimoNodo;
+			ultimoNodo = ultimoNodo->siguiente;
+		}
+        cantidadBebes++;			
+	}
+	
+	void imprimir(bool inverted){
+		if (inverted){
+			NodoHistorico * tmp = ultimoNodo;
+			while(tmp != NULL){
+				cout << tmp->tipoPadre<< " "<<tmp ->tipoMadre<<" "<< tmp->tipoBebe<< " -> ";
+				tmp = tmp->anterior;
+			}		
+		}
+		else{
+			NodoHistorico * tmp = primerNodo;
+			while(tmp != NULL){
+				cout << tmp->tipoPadre<< " "<<tmp ->tipoMadre<<" "<< tmp->tipoBebe<< " -> ";
+				tmp = tmp->siguiente;
+			}
+		}
+		cout << endl;
+	}
+	
+	//borrar al inicio: si esta vacia retorna null
+	NodoHistorico * borrar(){
+		NodoHistorico * borrado = primerNodo;
+		if (!isEmpty() && primerNodo == ultimoNodo)
+			primerNodo = ultimoNodo = NULL;
+		else if (!isEmpty()){
+			primerNodo = primerNodo->siguiente;
+			primerNodo->anterior = borrado->siguiente = NULL;	
+		}
+		return borrado;
+	}
+	
+	NodoHistorico * borrarAlFinal(){
+		NodoHistorico * borrado = ultimoNodo;
+		if (!isEmpty() && primerNodo == ultimoNodo)
+			primerNodo = ultimoNodo = NULL;
+		else if (!isEmpty()){
+			ultimoNodo = ultimoNodo->anterior;
+			ultimoNodo->siguiente = borrado->anterior = NULL;	
+		}
+		return borrado;
+	}
+	
+	
+	
+};
+
+
+
+
+void listdir(string initStrPath,ColaPedidos * cola,ListaPedidosEspeciales * lista){
 
     struct dirent * dir;
-    DIR * dp = opendir(initStrPath.c_str()); 
+    DIR * dp = opendir(initStrPath.c_str());
+     
     
     if(dp){
         while((dir = readdir(dp))!=NULL){
@@ -392,16 +369,25 @@ void listdir(string initStrPath){
                 initStrPath = checkPathString(initStrPath);
                 strPath = initStrPath+"/"+dir->d_name;
                 //cout<<strPath<<endl;
-                cout<<"\n ------ \n"<<endl;
+                //cout<<"\n ------ \n"<<endl;
                 string cP =colorPapa(strPath);
                 int lenPadre = longitudPapa(strPath);
                 string cM = colorMama(strPath,lenPadre); // recibe len de tipo padre para cortar string a partir de ahi
                 //ocurre prblema por crear bebes con mismo identificador en loop?
-                Bebe * b1 = new Bebe(cM,cP);
-                b1->imprimirPiel();
-                listdir(strPath);
+
+                Padres * p1 = new Padres(cP,cM);
+                string colorHijo = p1->probabilidadHijo();
+                Bebe * b1 = new Bebe(colorHijo);
+                //cola->encolarBebe(b1);
+                //cola->imprimir();
+
+                //NodoHistorico * n1 = new NodoHistorico(cM,cP,colorHijo);
+                lista->insertarAlFinal(cM,cP,colorHijo); // crea nuevo nodo y lo mete a la lista
+                
+
+                listdir(strPath,cola,lista);
                 const int result = remove(strPath.c_str());
-                cout<<"\n ------ \n"<<endl;
+                //cout<<"\n ------ \n"<<endl;
                 
             }
         }
--- main.cpp
+++ main.cpp
@@ -1,39 +1,13 @@
 #include "bebes.cpp"
 
+
 int main(int argc,char*argv[]){
+    ColaPedidos * c1 = new ColaPedidos ();
+    ListaPedidosEspeciales * l1 = new ListaPedidosEspeciales();
 
-    //listdir("C:\\Users\\mauar\\Desktop\\CODE\\Proyecto_ED_BabiesFactory\\pedidosEspeciales");
-
-    /*
-    int matriz[4][4];
-    for(int i =0;i<=3;i++){
-        for(int j = 0 ;j<=3;j++){
-            
-            matriz[i][j] = 20;
-        }
-    }
-    
-
-    const int matriz[4][4] = 
-    {
-        {1,2,3,4},
-        {5,6,7,8},
-        {9,10,11,12},
-        {13,14,15,16}
-    };
-
-    for(int i =0;i<=3;i++){
-        for(int j = 0 ;j<=3;j++){
-            cout<<"\n fila no: "<<i<<endl;
-            cout<<"columna no: "<<j<<endl;
-            cout<<matriz[i][j]<<endl;
-            cout<<"\n ------- \n"<<endl;
-        }
-    }
-    return 0;
-    */
-   Padres * p1 = new Padres("Castano","Morena");
-   cout<<"Hijo: "<<p1->probabilidadHijo()<<endl;
+    listdir("C:\\Users\\mauar\\Desktop\\CODE\\Proyecto_ED_BabiesFactory\\pedidosEspeciales",c1,l1);
+    //l1->imprimir(false);
+    cout<<"\ncant bebes: "<<l1->cantidadBebes<<endl;
    
 
 }
